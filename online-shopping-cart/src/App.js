3815f2b0e6172fc3d742ba9a52b38950ebf80504
Sem8 <mssemmi8@gmail.com>
2019-05-12 03:48:55 -0400

    homepage rendering products data of product image, name, description and price

--- online-shopping-cart/src/App.js
+++ online-shopping-cart/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import Router from './Router';
+
+const Navigation = (props) => <nav>
+  <ul>
+    <li><NavLink to='/'>Home</NavLink></li>
+    <li><NavLink to='/cart'>Cart</NavLink></li>
+  </ul>
+</nav>
 
 function App() {
   return (
     <div className="App">
-      <h1>My shopping cart</h1>
+      <Navigation />
+
+      <Router />
     </div>
   );
 }
