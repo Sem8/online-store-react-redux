0071d9054c65145f3a4282a0f1e419796da7ae44
Sem8 <mssemmi8@gmail.com>
2019-05-12 17:44:47 -0400

    products rendered to page with some styling

--- online-shopping-cart/src/features/product-listing/index.js
+++ online-shopping-cart/src/features/product-listing/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ProductListItem from './product-list-item';
 
 export default function ProductListing(props) {
-    return <div>
+    return <div className='product-listing'>
         {
             props.products.map(product => 
                 <ProductListItem product={product} />)
